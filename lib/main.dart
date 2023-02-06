989d709ca5295594be32c91b3988dbf086845782
Martin Rodriguez <martin.rodriguez@naranja.com>
2023-02-06 05:20:15 -0300

    Refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  void playSound(int sound)  async {
+  void playSound(int sound) async {
     final player = AudioPlayer();
     await player.play(AssetSource('note$sound.wav'));
   }
+
+  Widget buildKey(Color color, int keyNumber) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        onPressed: () {
+          playSound(keyNumber);
+        },
+        child: Container(color: color),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,80 +34,13 @@ class MyApp extends StatelessWidget {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(
-                child: TextButton(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero
-                    ),
-                    child: Container(
-                        color: Colors.red,
-                    ),
-                    onPressed: () {
-                      playSound(1);
-                    },
-              ),
-            ),
-            Expanded(
-
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero
-                  ),
-                  onPressed: () {
-                    playSound(2);
-                  },
-              child: Container(color: Colors.orange)),
-            ),
-            Expanded(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero
-                  ),
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Container(color: Colors.yellow)),
-            ),
-            Expanded(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero
-                  ),
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Container(color: Colors.green)),
-            ),
-            Expanded(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero
-                  ),
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Container(color: Colors.teal)),
-            ),
-            Expanded(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero
-                  ),
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Container(color: Colors.blue)),
-            ),
-            Expanded(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero
-                  ),
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Container(color: Colors.purple)),
-            ),
+            buildKey(Colors.red, 1),
+            buildKey(Colors.orange, 2),
+            buildKey(Colors.yellow, 3),
+            buildKey(Colors.green, 4),
+            buildKey(Colors.teal, 5),
+            buildKey(Colors.blue, 6),
+            buildKey(Colors.purple, 7),
           ],
         ),
       ),
